5b94018decba62ad9d43c990d7a2b4bb16258ebb
bankolecruise <samsticks150@gmail.com>
2022-07-19 03:51:38 +0530

    querrysets

---
+++ I4G006654ZCZ/links/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from rest_framework import serializers
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView, DestroyAPIView
+from .models import Link
+
+from .serializers import LinkSerializer
+
+
+class PostListApi(ListAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostCreateApi(CreateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostUpdateApi(UpdateAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDetailApi(RetrieveAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+
+class PostDeleteApi(DestroyAPIView):
+    queryset = Link.objects.filter(active=True)
+    serializer_class = LinkSerializer
+
+# Create your views here.
